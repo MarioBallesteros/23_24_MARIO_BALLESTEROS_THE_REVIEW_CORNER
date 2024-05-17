e47b92ea0ba45b5dac5565771bcb2760f52ec977
MarioBallesteros <16maarioo16@gmail.com>
2024-05-17 16:19:37 +0200

    asd

--- js/servidor.js
+++ js/servidor.js
@@ -2,13 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // Ruta al archivo de claves JSON
+const serviceAccount = require('./serviceAccountKey.json');
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.set('view engine', 'ejs'); // Establece EJS como el motor de plantillas
+app.set('views', path.join(__dirname,"../")); // Asegúrate que la ruta es correcta y que los archivos .ejs están ahí
 
 // Inicializar Firebase Admin SDK
 admin.initializeApp({
@@ -18,25 +20,20 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-// Servir la página HTML básica
-app.use(express.static(path.join(__dirname, '../')));
-
+// Ruta principal que carga la vista 'index.ejs'
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.render('index'); // No es necesario especificar .ejs, asegúrate de tener una plantilla 'index.ejs'
 });
 
-// Servir la aplicación Flutter en la subruta /flutter
-app.use('/flutter', express.static(path.join(__dirname, '../thefluttercorner-1/build/web')));
-
 // Ruta para obtener datos de producto basado en categoría y ID
-app.get('/categoria/:categoria/:id', async (req, res) => {
+app.get('/producto/:categoria/:id', async (req, res) => {
     const { categoria, id } = req.params;
 
     try {
         const productSnapshot = await db.collection('categoria').doc(categoria).collection('productos').doc(id).get();
         if (productSnapshot.exists) {
-            // Enviar los datos a la página de producto, por ejemplo renderizando la página con un template
-            res.render('producto', { producto: productSnapshot.data() }); // Esto requeriría un motor de plantillas como ejs, pug, etc.
+            const producto = productSnapshot.data();
+            res.render('producto', { producto });
         } else {
             res.status(404).send('Producto no encontrado');
         }
@@ -46,6 +43,8 @@ app.get('/categoria/:categoria/:id', async (req, res) => {
     }
 });
 
+// Servir archivos estáticos, asegúrate de que la ruta es correcta
+app.use(express.static(path.join(__dirname, '../')));
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
